98b8a3bafd534c878a27658d605ee4eb63075dcd
Erick Lemos <ericklemoscosta@gmail.com>
2022-04-11 19:29:27 -0300

    modularizacao

--- apps/template/app/domain/src/main/java/com/spacesandbox/template/domain/models/Animal.java
+++ apps/template/app/domain/src/main/java/com/spacesandbox/template/domain/models/Animal.java
@@ -1,2 +1,16 @@
-package com.spacesandbox.template.domain.models;public class Animal {
+package com.spacesandbox.template.domain.models;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class Animal {
+
+    private String id;
+    private String nome;
+    private Integer patas;
+
 }
