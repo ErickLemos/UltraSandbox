bd904d72378ec68851605b188ac9412e9fc9ec36
Erick Lemos <ericklemoscosta@gmail.com>
2022-04-11 12:27:10 -0300

    ajustes na cdi

--- apps/template-gradle/src/repository/src/main/java/com/sandbox/template/repository/mappers/VersaoMapper.java
+++ apps/template-gradle/src/repository/src/main/java/com/sandbox/template/repository/mappers/VersaoMapper.java
@@ -3,11 +3,12 @@ package com.sandbox.template.repository.mappers;
 import com.sandbox.template.domain.Versao;
 import com.sandbox.template.repository.model.VersaoEntity;
 import org.mapstruct.Mapper;
-import org.mapstruct.MappingConstants;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.CDI)
+@Mapper(componentModel = "spring")
 public interface VersaoMapper {
+
     Versao toDomain(VersaoEntity entity);
 
     VersaoEntity toEntity(Versao domain);
+
 }
