0550246fef7cc060eb6e50aa3e673b4b748bd0c1
Erick Lemos <ericklemoscosta@gmail.com>
2022-04-11 22:24:16 -0300

    ajustes

--- apps/template-gradle/app/rest/src/main/java/com/spacesandbox/template/controllers/AnimalController.java
+++ apps/template-gradle/app/rest/src/main/java/com/spacesandbox/template/controllers/AnimalController.java
@@ -1,7 +1,6 @@
 package com.spacesandbox.template.controllers;
 
 import com.spacesandbox.template.core.repository.AnimalRepository;
-import com.spacesandbox.template.domain.models.Animal;
 import com.spacesandbox.template.mappers.AnimalDtoMapper;
 import com.spacesandbox.template.models.AnimalDto;
 import org.springframework.http.ResponseEntity;
@@ -14,21 +13,21 @@ import java.util.List;
 public record AnimalController(AnimalRepository animalRepository, AnimalDtoMapper mapper) {
 
     @GetMapping
-    public ResponseEntity<List<Animal>> getAll() {
+    public ResponseEntity<List<AnimalDto>> getAll() {
         var animais = animalRepository.findAll();
-        return ResponseEntity.ok(animais);
+        return ResponseEntity.ok(animais.stream().map(mapper::toDto).toList());
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<Animal> findById(@PathVariable String id) {
+    public ResponseEntity<AnimalDto> findById(@PathVariable String id) {
         var animal = animalRepository.findById(id);
-        return ResponseEntity.ok(animal);
+        return ResponseEntity.ok(mapper.toDto(animal));
     }
 
     @PostMapping
-    public ResponseEntity<Animal> save(@RequestBody AnimalDto dto) {
+    public ResponseEntity<AnimalDto> save(@RequestBody AnimalDto dto) {
         var animal = animalRepository.save(mapper.toDomain(dto));
-        return ResponseEntity.ok(animal);
+        return ResponseEntity.ok(mapper.toDto(animal));
     }
 
 }
