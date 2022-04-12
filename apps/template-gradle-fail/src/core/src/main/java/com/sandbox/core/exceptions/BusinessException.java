ace066c70d87aba31928a4519d46d135348d95f6
Erick Lemos <ericklemoscosta@gmail.com>
2022-04-11 21:26:02 -0300

    remoção de projeto antigo

--- apps/template-gradle-fail/src/core/src/main/java/com/sandbox/core/exceptions/BusinessException.java
+++
@@ -1,9 +0,0 @@
-package com.sandbox.core.exceptions;
-
-public class BusinessException extends Exception {
-
-    public BusinessException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-}
--- apps/template-gradle-fail/src/repository/src/main/java/com/sandbox/repository/repositorys/VersaoRepositoryMongo.java
+++
@@ -1,9 +0,0 @@
-package com.sandbox.repository.repositorys;
-
-import com.sandbox.repository.model.VersaoEntity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VersaoRepositoryMongo extends MongoRepository<VersaoEntity, String> {
-}
