2d9dc4c7c18c534164bf0e650bc8c4a7cdf34a5c
Erick Lemos <ericklemoscosta@gmail.com>
2022-05-03 17:36:49 -0300

    reforação

--- apps/planetas/app/repositorys/vida/src/main/java/com/ultrasandbox/planetas/repository/commands/SalvarVidaDadosCommand.java
+++ apps/planetas/app/repositorys/vida/src/main/java/com/ultrasandbox/planetas/repository/commands/SalvarVidaDadosCommand.java
@@ -1,5 +1,6 @@
 package com.ultrasandbox.planetas.repository.commands;
 
+import com.ultrasandbox.planetas.core.exceptions.FactoryException;
 import com.ultrasandbox.planetas.domain.models.enums.TipoDeVida;
 import com.ultrasandbox.planetas.repository.models.AnimalDadosEntity;
 import com.ultrasandbox.planetas.repository.models.PlantaDadosEntity;
@@ -11,15 +12,14 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class SalvarVidaDadosCommand {
-
-    private final VidaPlantaDadosMongoRepository dadosPlantaRepository;
-    private final VidaAnimalDadosMongoRepository dadosAnimalRepository;
+public record SalvarVidaDadosCommand(
+        VidaPlantaDadosMongoRepository dadosPlantaRepository,
+        VidaAnimalDadosMongoRepository dadosAnimalRepository) {
 
     public VidaDadosEntity process(VidaDadosEntity dados, TipoDeVida tipo) {
         if (tipo == TipoDeVida.ANIMAL) return dadosAnimalRepository.save((AnimalDadosEntity) dados);
         if (tipo == TipoDeVida.PLANTA) return dadosPlantaRepository.save((PlantaDadosEntity) dados);
-        throw new RuntimeException();
+        throw new FactoryException();
     }
 
 }
