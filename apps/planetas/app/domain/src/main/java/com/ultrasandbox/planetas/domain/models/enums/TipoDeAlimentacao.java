b4e921c508dfc35699c0f70c30f0b139af3afa5e
Erick B. Lemos <ericklemoscosta@gmail.com>
2022-06-28 22:39:56 -0300

    anum

--- apps/planetas/app/domain/src/main/java/com/ultrasandbox/planetas/domain/models/enums/TipoDeAlimentacao.java
+++ apps/planetas/app/domain/src/main/java/com/ultrasandbox/planetas/domain/models/enums/TipoDeAlimentacao.java
@@ -1,7 +1,11 @@
 package com.ultrasandbox.planetas.domain.models.enums;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+import java.util.Arrays;
+
+@Getter
 @AllArgsConstructor
 public enum TipoDeAlimentacao {
     CARNIVORO("CARNIVORO"),
@@ -10,4 +14,16 @@ public enum TipoDeAlimentacao {
     FOTOSINTESE("FOTOSINTESE");
 
     private final String valor;
+
+    public static TipoDeAlimentacao entryOf(String valor) {
+        return Arrays.stream(TipoDeAlimentacao.values())
+                .filter(tipo -> tipo.getValor().equals(valor))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public static boolean exists(String valor) {
+        return Arrays.stream(TipoDeAlimentacao.values())
+                .anyMatch(tipo -> tipo.getValor().equals(valor));
+    }
 }
